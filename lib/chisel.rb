21d1788ab0fd574be4ef17f9092b2ffadcfd9997
Adam Caron <adamwcaron@gmail.com>
2015-06-08 11:17:23 -0600

    initial commit for chisel.rb and for the markdown doc

---
+++ lib/chisel.rb
@@ -0,0 +1,3 @@
+# Get the markdown document
+# Pass the string to render.rb
+# Takes what render returned and outputs it as one HTML file
---
+++ lib/document.md
@@ -0,0 +1,3 @@
+# This is the Header
+
+This is my favorite paragraph ever. It's such a nice paragraph.
