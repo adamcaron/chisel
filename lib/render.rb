f7cdcfead282d4907df45b902c120e62c5fa26e7
Adam Caron <adamwcaron@gmail.com>
2015-06-08 11:16:36 -0600

    refactors psuedo code

--- lib/render.rb
+++ lib/render.rb
@@ -1,9 +1,4 @@
-# Chisel reads the markdown file
-
-# Get the initial markdown document
-# Separate the file into chunks of text (delineated by blank lines)
+# Separate the document into chunks of text (delineated by blank lines)
 # Classify those chunks of text (by header or paragraph)
 # Based on what type of chunk it is, send it to it's respective renderer.
 # Combine all chunks back into one, gigantic string
-
-# Takes what render returned and outputs it as one HTML file
