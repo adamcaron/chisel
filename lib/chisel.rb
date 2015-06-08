afc941234145a16ce3c36c55a669d641f532deca
Adam Caron <adamwcaron@gmail.com>
2015-06-08 12:11:00 -0600

    reads markdown file, then passes contents to renderer

--- lib/chisel.rb
+++ lib/chisel.rb
@@ -1,3 +1,9 @@
+require_relative 'renderer'
+
 # Get the markdown document
+doc = File.read(ARGV[0])
+
 # Pass the string to render.rb
+renderer = Renderer.new(doc)
+
 # Takes what render returned and outputs it as one HTML file
--- lib/render.rb
+++
@@ -1,4 +0,0 @@
-# Separate the document into chunks of text (delineated by blank lines)
-# Classify those chunks of text (by header or paragraph)
-# Based on what type of chunk it is, send it to it's respective renderer.
-# Combine all chunks back into one, gigantic string
