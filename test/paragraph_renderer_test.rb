07bd2f91cba342ea0a92ed8c7b18330f2aedbd80
Adam Caron <adamwcaron@gmail.com>
2015-06-09 13:48:00 -0600

    tests it transforms single-line & multi-line paragraphs

---
+++ test/paragraph_renderer_test.rb
@@ -0,0 +1,33 @@
+require './test/test_helper'
+require './lib/paragraph_renderer'
+
+class TestParagraphRenderer < Minitest::Test
+  def test_it_exists
+    assert ParagraphRenderer
+  end
+
+  def test_it_transforms_a_single_line_to_one_paragraph
+
+    input_text = "This is the first line of the paragraph."
+    expected_text = <<-EOS
+<p>
+    This is the first line of the paragraph.
+</p>
+EOS
+    assert_equal expected_text, ParagraphRenderer.new(input_text).transform
+  end
+
+  def test_it_transforms_a_multi_line_paragraph
+    input_text = <<-EOS
+This is the first line of the paragraph.
+This is the second line of the same paragraph.
+EOS
+    expected_text = <<-EOS
+<p>
+    This is the first line of the paragraph.<br>
+    This is the second line of the same paragraph.
+</p>
+EOS
+    assert_equal expected_text, ParagraphRenderer.new(input_text).transform
+  end
+end
