3945cfa054dff828e55def9167a9be2db38b8d2a
Adam Caron <adamwcaron@gmail.com>
2015-06-09 18:50:04 -0600

    tests it handles lines that start with # and ignores other lines; and tests it transforms # to h1, and ## to h2

--- test/header_renderer_test.rb
+++ test/header_renderer_test.rb
@@ -2,25 +2,25 @@ require_relative 'test_helper'
 require_relative '../lib/header_renderer'
 
 class TestHeaderRenderer < Minitest::Test
-  # identification happens in the chunker
-  # so HeaderRenderer will be relieved of this responsibility
-  def test_it_identifies_octothorpes
-    chunked_document = HeaderRenderer.new(["# Here's a header", "Not a header"])
-    assert_equal ["# Here's a header"], chunked_document.find_headers
+  def test_it_handles_lines_that_start_with_hash
+    assert HeaderRenderer.new.handles?("# Here's a header")
+  end
+
+  def test_it_doesnt_handle_lines_without_a_hash
+    refute HeaderRenderer.new.handles?("Not a header")
+  end
+
+  def test_it_doesnt_handle_mid_line_hashes
+    refute HeaderRenderer.new.handles?("Just a # paragraph")
   end
 
   def test_it_transforms_one_octothorpe_into_an_h1
-    chunked_document = HeaderRenderer.new(["# Here's My Title"])
-    assert_equal ["<h1>Here's My Title</h1>"], chunked_document.transform
+    renderer = HeaderRenderer.new
+    assert_equal "<h1>Here's My Title</h1>", renderer.transform("# Here's My Title")
   end
 
   def test_it_transforms_two_octothorpes_into_h2
-    chunked_document = HeaderRenderer.new(["## Here's an h2 Subheader"])
-    assert_equal ["<h2>Here's an h2 Subheader</h2>"], chunked_document.transform
-  end
-
-  def test_it_doesnt_transform_midline_octothorpes
-    chunked_document = HeaderRenderer.new(["# Here's My Title", "Just a # paragraph"])
-    assert_equal ["<h1>Here's My Title</h1>"], chunked_document.transform
+    renderer = HeaderRenderer.new
+    assert_equal "<h2>Here's an h2 Subheader</h2>", renderer.transform("## Here's an h2 Subheader")
   end
 end
