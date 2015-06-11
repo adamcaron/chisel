e16f18f92f71fc16d437977168a34cd4006c7aec
Adam Caron <adamwcaron@gmail.com>
2015-06-11 09:26:43 -0600

    refactors emphasis

--- lib/emphasis_renderer.rb
+++ lib/emphasis_renderer.rb
@@ -4,16 +4,10 @@ class EmphasisRenderer
   end
 
   def transform(chunk)
-    count = 0
-    chunk.chars.map do |char|
-      if char == "*" && count == 0
-        count += 1
-        "<em>"
-      elsif char == "*" && count == 1
-        "</em>"
-      else
-        char
-      end
-    end.join
+    while chunk.include?("*")
+      chunk.sub!("*", "<em>")
+      chunk.sub!("*", "</em>")
+    end
+    return chunk
   end
 end
