30d158ecb8fafb73381e21a5779bc7ed3b042cda
Adam Caron <adamwcaron@gmail.com>
2015-06-11 11:44:45 -0600

    sets up tests for chunker

--- test/chunker_test.rb
+++ test/chunker_test.rb
@@ -2,5 +2,26 @@ require './test/test_helper'
 require './lib/chunker'
 
 class ChunkerTest < Minitest::Test
+  def test_it_splits_a_md_doc_into_chunks
+  end
 
+  def test_it_renders_md_headers_to_html
+    skip
+  end
+
+  def test_it_renders_md_paragraphs_to_html
+    skip
+  end
+
+  def test_it_renders_md_emphasis_to_html_em
+    skip
+  end
+
+  def test_it_renders_bold_md_as_strong_html
+    skip
+  end
+
+  def test_it_joins_rendered_chunks_into_an_html_doc
+    skip
+  end
 end
